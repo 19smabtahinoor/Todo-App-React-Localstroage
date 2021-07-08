cbff5e748c436f6f2195af909936d89a4d9d2a45
19smabtahinoor <abtahinookabid@gmail.com>
2021-07-08 12:23:54 +0600

    Preloader Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import Todo from './Todo'
+import SyncLoader from "react-spinners/SyncLoader";
+import { css } from "@emotion/react";
+
+const override = css`
+  display: flex;
+  flex-direction:row;
+  justify-content:center;
+  align-items:center;
+  min-height:100vh;
+  margin: 0 auto;
+  width:100%;
+`;
 
 const App = () => {
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
+
   return (
     <>
-      <div className="App">
-        <Todo/>
-      </div>
+      {loading ?
+        <SyncLoader color={'#4A90E2'} loading={loading} size={20} css={override} />
+        :
+        <div className="App">
+          <Todo />
+        </div>
+
+      }
     </>
   )
 }
